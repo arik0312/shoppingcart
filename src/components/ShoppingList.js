84e0991f811959caccc74d437d64cda45455e58d
arik0312 <beautyplus30@gmail.com>
2023-04-03 09:06:07 +0300

    ppp

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Row,
@@ -8,34 +8,75 @@ import {
   ButtonGroup,
   Form,
   Modal,
+  InputGroup,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faAdd } from "@fortawesome/free-solid-svg-icons";
-import { faClose } from "@fortawesome/free-solid-svg-icons";
-import { faCashRegister } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import {
+  faMinus,
+  faPlus,
+  faTrash,
+  faAdd,
+  faClose,
+  faEdit,
+  faSave,
+  faPerson,
+} from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
+
+import ManIcon from "../icons/man.png";
+import trolley from "../icons/trolley.png";
+
 const ShoppingList = () => {
   const [items, setItems] = useState([]);
 
   const [total, setTotal] = useState(0);
   const [totalqty, setTotalQty] = useState(0);
-  const [newProduct, setNewProduct] = useState({ name: "", price: 0, qty: 0 });
+  const [newProduct, setNewProduct] = useState({
+    itemname: "",
+    price: 0,
+    qty: 0,
+  });
   const [showAddProductModal, setShowAddProductModal] = useState(false);
-  const handleAddToCart = (index) => {
+
+  useEffect(() => {
+    fetch("http://localhost:3001/getshoppinitems")
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data);
+      })
+      .catch((error) => {
+        msgerror();
+      });
+  }, []);
+
+  function msgerror() {
+    toast.error("שגיאה", {
+      position: "top-center",
+      autoClose: 800,
+      hideProgressBar: true,
+      closeOnClick: false,
+      rtl: true,
+    });
+  }
+
+  const handleedit = (index) => {
     const item = items[index];
-    const sum = parseFloat(item.price) + parseFloat(total);
-    const sumqty = parseFloat(item.qty) + parseFloat(totalqty);
-    setTotal(sum);
+    const id = item.id;
+    const itemname = item.itemname;
+    const price = item.price;
+    const qty = item.qty;
+    setNewProduct({ id, itemname, price, qty });
+    setShowAddProductModal(true);
   };
 
   const handleDeleteItem = (index) => {
     Swal.fire({
-      text: "למחוק את הפריט מהרשימה" + " " + "?" + items[index].name,
-      icon: "warning",
+      text: "למחוק את הפריט מהרשימה" + " " + "?" + items[index].itemname,
+      icon: "question",
       showCancelButton: true,
       confirmButtonColor: "green",
       cancelButtonColor: "#3085d6",
@@ -44,10 +85,24 @@ const ShoppingList = () => {
       allowOutsideClick: false,
     }).then((result) => {
       if (result.isConfirmed) {
-        setItems(items.filter((item, i) => i !== index));
-        setTotal(total - parseFloat(items[index].price));
-        setTotalQty(totalqty - items[index].qty);
-        msgdelete();
+        const itemname = items[index].itemname;
+        fetch(`http://localhost:3001/DeleteShoppingListItem/${itemname}`, {
+          method: "DELETE",
+        })
+          .then((response) => {
+            if (response.ok) {
+              setItems(items.filter((item, i) => i !== index));
+              setTotal(total - parseFloat(items[index].price));
+              setTotalQty(totalqty - items[index].qty);
+              msgdelete();
+            } else {
+              throw new Error("Failed to delete item");
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            console.log(error);
+          });
       }
     });
   };
@@ -72,6 +127,16 @@ const ShoppingList = () => {
     }
   };
 
+  function msgupitemexsits() {
+    toast.info("הפריט כבר נמצא במערכת", {
+      position: "top-center",
+      autoClose: 800,
+      hideProgressBar: true,
+      closeOnClick: false,
+      rtl: true,
+    });
+  }
+
   function msgdelete() {
     toast.info("הפריט נמחק בהצלחה", {
       position: "top-left",
@@ -90,8 +155,19 @@ const ShoppingList = () => {
       closeOnClick: false,
       rtl: true,
     });
+    handleresetfields();
   }
 
+  function msgupdatedSucceful() {
+    toast.success("הפריט עודכן בהצלחה", {
+      position: "top-center",
+      autoClose: 800,
+      hideProgressBar: true,
+      closeOnClick: false,
+      rtl: true,
+    });
+    handleresetfields();
+  }
   function handleResetCart() {
     setItems([]);
     setTotal(0);
@@ -99,17 +175,111 @@ const ShoppingList = () => {
   }
 
   function handleresetfields() {
-    setNewProduct({ name: "", price: 0, qty: 0 });
+    setNewProduct({ itemname: "", price: 0, qty: 0 });
     setShowAddProductModal(false);
   }
 
+  // const handleAddNewProduct = () => {
+  //   // Check if item already exists
+  //   fetch(`http://localhost:3001/CheckItem/${newProduct.itemname}`)
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       } else {
+  //         throw new Error("Failed to check item");
+  //       }
+  //     })
+  //     .then((data) => {
+  //       if (data.length > 0) {
+  //         // Item already exists, show error message
+  //         msgAddedFail("Item already exists");
+  //       } else {
+  //         // Item does not exist, add to shopping list
+  //         if (!newProduct.id) {
+  //           // Adding new item
+  //         } else {
+  //           // Updating existing item
+  //           fetch(
+  //             `http://localhost:3001/UpdateShoppingListItem/${newProduct.id}`,
+  //             {
+  //               method: "PUT",
+  //               headers: {
+  //                 "Content-Type": "application/json",
+  //               },
+  //               body: JSON.stringify(newProduct),
+  //             }
+  //           )
+  //             .then((response) => {
+  //               if (response.ok) {
+  //                 setItems(
+  //                   items.map((item) =>
+  //                     item.id === newProduct.id ? newProduct : item
+  //                   )
+  //                 );
+  //                 setShowAddProductModal(false);
+  //                 msgupdatedSucceful();
+  //               } else {
+  //                 throw new Error("Failed to update product");
+  //               }
+  //             })
+  //             .catch((error) => {
+  //               console.error(error);
+  //               msgAddedFail();
+  //             });
+  //         }
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //       msgAddedFail();
+  //     });
+  // };
+
   const handleAddNewProduct = () => {
-    setItems([...items, newProduct]);
-    setTotal(total + parseFloat(newProduct.price));
-    setTotalQty(totalqty + 1);
-    setNewProduct({ name: "", price: 0, qty: 0 });
-    setShowAddProductModal(false);
-    msgAddedSucceful();
+    // Check if item already exists
+    fetch(`http://localhost:3001/CheckItem/${newProduct.itemname}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to check item");
+        }
+      })
+      .then((data) => {
+        if (data.length > 0) {
+          // Item already exists, show error message
+          msgupitemexsits();
+        } else {
+          // Item does not exist, add to shopping list
+          fetch("http://localhost:3001/AddNewShoppingList", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newProduct),
+          })
+            .then((response) => {
+              if (response.ok) {
+                msgAddedSucceful();
+                setItems([...items, newProduct]);
+              } else {
+                throw new Error("Failed to add product");
+              }
+            })
+            .catch((error) => {
+              console.error(error);
+              msgAddedFail();
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        msgAddedFail();
+      });
+  };
+
+  const msgAddedFail = () => {
+    alert("Failed to add product. Please try again later.");
   };
 
   return (
@@ -130,27 +300,35 @@ const ShoppingList = () => {
               <tbody>
                 {items.map((item, index) => (
                   <tr key={index}>
-                    <td>{item.name}</td>
+                    <td>{item.itemname}</td>
                     <td>{item.qty}</td>
                     <td>{item.price}</td>
                     <td>
-                      <Button
-                        variant="primary"
-                        onClick={() => handleAddToCart(index)}
-                      >
-                        <FontAwesomeIcon
-                          icon={faCashRegister}
-                          className="mr-5"
-                        />
-                        Add to Total
-                      </Button>
-                      <Button
-                        variant="danger"
-                        onClick={() => handleDeleteItem(index)}
-                      >
-                        Remove
-                        <FontAwesomeIcon icon={faTrash} className="mr-2" />
-                      </Button>
+                      <div>
+                        <span
+                          style={{ width: "10px", display: "inline-block" }}
+                        ></span>
+                        <Button
+                          variant="danger"
+                          onClick={() => handleDeleteItem(index)}
+                        >
+                          Remove
+                          <FontAwesomeIcon icon={faTrash} className="mr-2" />
+                        </Button>
+                        <span
+                          style={{ width: "10px", display: "inline-block" }}
+                        ></span>
+                        <Button
+                          variant="success"
+                          onClick={() => handleedit(index)}
+                        >
+                          <span
+                            style={{ width: "10px", display: "inline-block" }}
+                          ></span>
+                          edit
+                          <FontAwesomeIcon icon={faEdit} className="mr-2" />
+                        </Button>
+                      </div>
                     </td>
                   </tr>
                 ))}
@@ -160,22 +338,6 @@ const ShoppingList = () => {
         </Row>
         <Row>
           <Col>
-            <ButtonGroup>
-              <Button
-                variant="danger"
-                className="mr-2"
-                onClick={handleResetCart}
-              >
-                Reset cart
-              </Button>
-              <Button
-                variant="success"
-                className="mr-2"
-                onClick={() => setTotal(0)}
-              >
-                Reset amount total
-              </Button>
-            </ButtonGroup>
             <Button
               variant="info"
               className="ml-2"
@@ -194,12 +356,17 @@ const ShoppingList = () => {
           backdrop="static"
         >
           <Modal.Header
-            style={{ textAlign: "center", backgroundColor: "green" }}
+            style={{ textAlign: "center", backgroundColor: "dodgerblue" }}
           >
             <Modal.Title
               style={{ textAlign: "center", color: "white", width: "100%" }}
             >
-              הוספת פריט חדש
+              <img src={trolley} style={{ marginRight: "10px" }} />
+              <span style={{ marginLeft: "10px" }}>
+                {newProduct.id
+                  ? `עדכן פריט ${newProduct.itemname}`
+                  : "הוספת פריט חדש"}
+              </span>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
@@ -210,9 +377,9 @@ const ShoppingList = () => {
                   type="text"
                   placeholder="הקלד את שם הפריט"
                   autoFocus={true}
-                  value={newProduct.name}
+                  value={newProduct.itemname}
                   onChange={(e) =>
-                    setNewProduct({ ...newProduct, name: e.target.value })
+                    setNewProduct({ ...newProduct, itemname: e.target.value })
                   }
                 />
               </Form.Group>
@@ -226,37 +393,54 @@ const ShoppingList = () => {
                   onChange={(e) =>
                     setNewProduct({ ...newProduct, price: e.target.value })
                   }
-                  // onKeyDown={(e) => {
-                  //   const regex = /^[0-9\b]+$/; // only allow numeric characters
-                  //   if (!regex.test(e.key)) {
-                  //     e.preventDefault(); // prevent non-numeric characters from being entered
-                  //   }
-                  // }}
                   onContextMenu={(e) => {
-                    e.preventDefault(); // Prevent the right-click context menu from appearing
+                    e.preventDefault();
                   }}
                 />
               </Form.Group>
               <Form.Group controlId="formqty">
                 <Form.Label>כמות</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter qty"
-                  value={newProduct.qty}
-                  maxLength={3}
-                  onChange={(e) =>
-                    setNewProduct({ ...newProduct, qty: e.target.value })
-                  }
-                  // onKeyDown={(e) => {
-                  //   const regex = /^[0-9\b]+$/; // only allow numeric characters
-                  //   if (!regex.test(e.key)) {
-                  //     e.preventDefault(); // prevent non-numeric characters from being entered
-                  //   }
-                  // }}
-                  onContextMenu={(e) => {
-                    e.preventDefault(); // Prevent the right-click context menu from appearing
-                  }}
-                />
+                <InputGroup>
+                  <Button
+                    variant="secondary"
+                    onClick={() =>
+                      setNewProduct({
+                        ...newProduct,
+                        qty: parseInt(newProduct.qty) - 1,
+                      })
+                    }
+                  >
+                    <FontAwesomeIcon icon={faMinus} />
+                  </Button>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter qty"
+                    value={newProduct.qty}
+                    maxLength={3}
+                    onChange={(e) =>
+                      setNewProduct({ ...newProduct, qty: e.target.value })
+                    }
+                    onKeyDown={(e) => {
+                      if (isNaN(parseInt(e.key))) {
+                        e.preventDefault();
+                      }
+                    }}
+                    onContextMenu={(e) => {
+                      e.preventDefault();
+                    }}
+                  />
+                  <Button
+                    variant="secondary"
+                    onClick={() =>
+                      setNewProduct({
+                        ...newProduct,
+                        qty: parseInt(newProduct.qty) + 1,
+                      })
+                    }
+                  >
+                    <FontAwesomeIcon icon={faPlus} />
+                  </Button>
+                </InputGroup>
               </Form.Group>
             </Form>
           </Modal.Body>
@@ -269,8 +453,12 @@ const ShoppingList = () => {
             </Button>
             <Button variant="primary" onClick={checkhandleAddNewProduct}>
               <div className="d-flex align-items-center">
-                <FontAwesomeIcon icon={faAdd} />
-                <span style={{ marginLeft: "10px" }}>Add Product</span>
+                <FontAwesomeIcon icon={faSave} />
+                <span style={{ marginLeft: "10px" }}>
+                  {newProduct.id
+                    ? `Update Product ${newProduct.id}`
+                    : "Add Product"}
+                </span>
               </div>
             </Button>
           </Modal.Footer>
